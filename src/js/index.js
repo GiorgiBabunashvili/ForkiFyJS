edd559252346c57d89e0c3558b58e87a433f0271
shanME <giorgie103188@gmail.com>
2024-07-31 16:58:58 +0400

    base file

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
-// Global app controller
+import { elements } from "./view/base";
+
+elements.searchForm.addEventListener("submit", () => {});
+
+// `http://forkify-api.herokuapp.com/api/search?q=${this.query}`
