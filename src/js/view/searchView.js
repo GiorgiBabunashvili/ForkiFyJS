149412bd72ea3a19501a0091d680d35d16e26da6
shanME <giorgie103188@gmail.com>
2024-07-31 19:10:44 +0400

    limit title

--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -3,6 +3,21 @@ import { elements } from "./base";
 export const getInput = () => elements.searchInput.value;
 export const clearInput = () => (elements.searchInput.value = "");
 
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+
+    return `${newTitle.join(" ")}...`;
+  }
+  return title;
+};
+
 const renderRecipe = (recipe) => {
   const markup = `
         <li>
@@ -11,12 +26,16 @@ const renderRecipe = (recipe) => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(
+                      recipe.title
+                    )}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
         </li>
     `;
+
+  elements.searchResultList.insertAdjacentHTML("beforeend", markup);
 };
 
 export const renderResult = (recipe) => {
