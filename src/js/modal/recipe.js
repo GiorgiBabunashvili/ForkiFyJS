2779c68ceb92783ca518b1d14e2168c153aaf353
shanME <giorgie103188@gmail.com>
2024-08-03 12:56:08 +0400

    ui

--- src/js/modal/recipe.js
+++ src/js/modal/recipe.js
@@ -44,41 +44,47 @@ export default class Recipe {
       const unitShorts = ["tbsp", "tbsp", "oz", "oz", "tsp", "tsp", "cup"];
       const units = [...unitShorts, "kg", "g", "pound"];
 
-      //1. uniform units
+      // 1. uniform unit
       let ingredient = el.toLowerCase();
       unitLong.forEach((unit, i) => {
         ingredient = ingredient.replace(unit, unitShorts[i]);
       });
 
-      //2. remove parentheses
+      // 2. remove paranethses (frchxilebi)
       ingredient = ingredient.replace(/ *\([^)]*\) */g, " ");
 
-      //3. parse ingredients into unit count unit ingredient
+      // 3. parse ingredients into cound, unit and ingredients
+      // 1/2 tsp salt => ['1/2', 'tsp', 'salt']
       const arrIng = ingredient.split(" ");
       const unitIndex = arrIng.findIndex((value) => units.includes(value));
 
       let objIng;
       if (unitIndex > -1) {
+        // there is a unit
+        // ex: 4 1/2; ['4', '1/2'] => 4.5
+        // ex: 4; ['4'] => 4
+        // ex: 1/2; ['1/2'] => 0.5
         const arrCount = arrIng.slice(0, unitIndex);
         let count;
         if (arrCount.length === 1) {
           count = eval(arrIng[0]);
         } else {
-          count = eval(arrCount.json("+"));
+          count = eval(arrCount.join("+")); // ['4', '1/2'] => '4 + 1/2'
         }
-
         objIng = {
-          count,
+          count, //count: count;
           unit: arrIng[unitIndex],
-          ingredient: arrIng.slice(unitIndex + 1).json(" "),
+          ingredient: arrIng.slice(unitIndex + 1).join(" "),
         };
       } else if (parseInt(arrIng[0], 10)) {
+        // there is No unit, but number
         objIng = {
           count: +arrIng[0],
           unit: "",
           ingredient: arrIng.slice(1).join(" "),
         };
       } else if (unitIndex === -1) {
+        //there is not unit
         objIng = {
           count: 1,
           unit: "",
@@ -88,19 +94,6 @@ export default class Recipe {
 
       return objIng;
     });
-
     this.ingredients = newIngredients;
   }
 }
-
-// const unitLong = [
-//   "tablespoons",
-//   "tablespoon",
-//   "ounces",
-//   "ounce",
-//   "teaspoons",
-//   "teaspoon",
-//   "cups",
-// ];
-// const unitShorts = ["tbsp", "tbsp", "oz", "oz", "tsp", "tsp", "cup"];
-// const units = [...unitShorts, "kg", "g", "pound"]
